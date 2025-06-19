075f1108d094e3dd6af8dbf500d02b3adf909d64
madusilva90 <maria.eduarda4@aluno.ifsp.edu.br>
2025-06-18 21:26:03 -0300

    Add files via upload

---
+++ ex26/exercicio26.js
@@ -0,0 +1,6 @@
+function borboletas(imagem) {
+imagem.src = "borboleta-decorativa-rosa-isolada-em-fundo-branco_1322553-45353.avif";
+}
+function borboleta(imagem) {
+imagem.src = "blue-butterfly-isolated-illustration-ai-generative-png.webp";
+}
