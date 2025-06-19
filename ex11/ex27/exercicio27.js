a5fd660f6365c48ad6ffd87109d4f55ed6853c4c
madusilva90 <maria.eduarda4@aluno.ifsp.edu.br>
2025-06-19 15:02:07 -0300

    Delete ex11/ex27/exercicio27.js

--- ex11/ex27/exercicio27.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById('campoTexto')
-const mensagem = document.getElementById('mensagem')
-input.addEventListener('keydown', function(event) {
-  const tecla = event.key;
-  mensagem.textContent = `Você pressionou: ${tecla}`;
-})
